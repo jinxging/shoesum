4483d9f31069138c3c8b6e25c18cc1b082bd2671
pc <pc@DESKTOP-NELI316>
2021-05-20 14:17:13 +0900

    JDBCtemplate , properties 생성 - 화영 -

---
+++ src/com/cart/service/CartService.java
@@ -0,0 +1,14 @@
+package com.cart.service;
+
+import com.cart.dao.CartDao;
+
+public class CartService {
+
+	
+	private CartDao dao = new CartDao();
+	
+	
+	
+	
+	
+}
---
+++ src/com/common/JDBCTemplate.java
@@ -0,0 +1,97 @@
+package com.common;
+
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class JDBCTemplate {
+	
+	
+	// connection 생성 
+	
+	public static Connection getConnection() {
+		
+		Properties prop = new Properties();
+		Connection conn = null;
+		
+		try {
+			String fileName = JDBCTemplate.class.getResource("/driver/driver.properties").getPath();
+			prop.load(new FileReader(fileName));
+			Class.forName(prop.getProperty("driver"));
+			conn = DriverManager.getConnection(prop.getProperty("url"),prop.getProperty("id"),prop.getProperty("password"));
+			conn.setAutoCommit(false);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	
+	// close 오버라이딩 
+	
+		public static void close(Connection conn) {
+			
+			try {
+				if(conn != null && !conn.isClosed()) {
+					conn.close();
+				} 
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		public static void close(PreparedStatement pstmt) {
+			
+			try {
+				if(pstmt!=null && !pstmt.isClosed()) {
+					pstmt.close();
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+		public static void close(ResultSet rs) {
+			
+			try {
+				if(rs!=null && !rs.isClosed()) {
+					rs.close();
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	
+	// commit
+		
+		public static void commit(Connection conn) {
+			
+			try {
+				if(conn!=null && !conn.isClosed()) {
+					conn.commit();
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	
+	// rollback
+
+		public static void rollback(Connection conn) {
+			try {
+				if(conn!=null && !conn.isClosed()) {
+					conn.rollback();
+				} 
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+}
